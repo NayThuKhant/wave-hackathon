89a51a5553964582bda8edc461ef1545ec1f9892
Nay Thu Khant <naythukhant644@gmail.com>
2023-09-16 18:42:15 +0630

    Address accessor added

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,7 +11,16 @@ class Address extends Model
 {
     use HasFactory;
 
+    protected $appends = ['full_address'];
+
     protected $fillable = ['user_id', 'floor', 'street', 'township', 'city'];
+
+    protected function fullAddress(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => "$this->floor, $this->street, $this->township, $this->city",
+        );
+    }
 
     public function user(): BelongsTo
     {
