d43d71e48587890e2d46cbe225f395f7c6f69ffd
Nay Thu Khant <naythukhant644@gmail.com>
2023-09-16 12:12:23 +0630

    Home Redesigned

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Enums\SystemStatus;
+use App\Models\Employee;
 use App\Models\User;
+use Symfony\Component\ErrorHandler\Error\UndefinedFunctionError;
 
 class EmployeeController extends Controller
 {
@@ -26,11 +28,31 @@ class EmployeeController extends Controller
                 $employee->rating = number_format($employee->orders->sum('rating') / $orderCount, 2);
                 return $employee;
             })->sortByDesc('rating')
-        ->values();
+            ->values();
 
         return response()->json([
             'message' => 'Employees have been retrieved.',
             'data' => $employees
+        ]);
+    }
+
+    public function show($id)
+    {
+        $user = User::where("id", $id)
+            ->select(['users.id as id', 'users.name', 'users.email', 'users.mobile_number', 'users.gender'])
+            ->firstOrFail();
+
+        $orders = $user->orders;
+        $orderCount = $orders->count();
+        if (!$orderCount) {
+            $user->rating = 0.0;
+        } else {
+            $user->rating = number_format($orders->sum('rating') / $orderCount, 2);
+        }
+
+        return response()->json([
+            'message' => 'Employee has been retrieved.',
+            'data' => $user->load('categories', 'orders')
         ]);
     }
 }
